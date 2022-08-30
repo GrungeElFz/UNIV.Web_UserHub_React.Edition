e8b0240dc6e22c52945bdf11018ed7ceea98379e
GrungeElFz <elf0114_@hotmail.com>
2022-08-30 13:26:23 -0500

    Checkpoint --> 4. Tutorial: Setting Current User

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import { Header } from './components';
+import { getUsers } from './api';
+
+const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        setUserList(users)
+      })
+      .catch(error => {
+        console.error(`Here is error: ${error}`)
+      });
+  }, []);
+
+  return (
+    <div id="App">
+      <Header userList={ userList } />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

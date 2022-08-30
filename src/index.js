a74d190c7feceb43862bd95c495e319f47bfdeca
GrungeElFz <elf0114_@hotmail.com>
2022-08-30 14:14:24 -0500

    Checkpoint --> 4. Tutorial: UserToDos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-import { Header } from './components';
-import { getUsers } from './api';
+import { Header, UserPosts } from './components';
+import { getUsers, getPostsByUser } from './api';
 
 const App = () => {
   const [userList, setUserList] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
 
   useEffect(() => {
     getUsers()
@@ -18,12 +19,28 @@ const App = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (!currentUser) {
+      setUserPosts([]);
+      return;
+    }
+
+    getPostsByUser(currentUser.id)
+      .then(posts => {
+        setUserPosts(posts);
+      })
+      .catch(error => {
+        console.error(`Here is error: ${error}`)
+      });
+  }, [currentUser]);
+
   return (
     <div id="App">
       <Header
         userList={ userList }
         currentUser={ currentUser }
         setCurrentUser={ setCurrentUser }/>
+      {currentUser ? <UserPosts userPosts={ userPosts } currentUser={ currentUser } /> : null }
     </div>
   );
 }
